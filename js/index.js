fb0ff1e20e6e484d1730993fce61ce25fb49f7d6
doej96 <doej96@naver.com>
2020-12-30 15:02:30 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -16,7 +16,20 @@ function mainBanner() {
 	});
 }
 
+function onMoNavi() {
+	$(this).next().toggleClass('active');
+}
+
+function onResize() {
+	var winWidth = $(window).width();
+	if(winWidth > 576) $('.mo-navi').hide();
+}
+
+
+$(window).resize(onResize).trigger('resize');
 
 $('.navi-wrap li').mouseenter(onNaviEnter);
 $('.navi-wrap li').mouseleave(onNaviLeave);
 mainBanner();
+
+$('.bars').click(onMoNavi);
