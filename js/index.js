beccf270e626a465dcc59c527eb107b7387419eb
doej96 <doej96@naver.com>
2020-12-30 13:13:24 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -6,6 +6,17 @@ function onNaviLeave() {
 	$(this).find('.sub-navi').removeClass('active');
 }
 
+function mainBanner() {
+	var swiper = new Swiper('.main-banner.swiper-container', {
+		slidesPerView: 1,
+		loop: 'true',
+		autoplay: {
+			delay : 3000,
+			},
+	});
+}
+
 
 $('.navi-wrap li').mouseenter(onNaviEnter);
-$('.navi-wrap li').mouseleave(onNaviLeave);
+$('.navi-wrap li').mouseleave(onNaviLeave);\
+mainBanner();
